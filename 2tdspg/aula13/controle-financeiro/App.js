a62257987501728f7ef27fcae0390ea264811aec
Antonio Carvalho <antoniorcn@hotmail.com>
2023-03-20 11:18:46 -0300

    2tdspg - aula 13 parte 3

--- 2tdspg/aula13/controle-financeiro/App.js
+++ 2tdspg/aula13/controle-financeiro/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ImageBackground, StyleSheet, Text,
-  TextInput, View, Button } from 'react-native';
+  TextInput, View, Button, ScrollView } from 'react-native';
 import coinsimg from './assets/coins.jpg';
 
 const estilos = StyleSheet.create({
@@ -13,17 +13,38 @@ const estilos = StyleSheet.create({
     borderBottomWidth: 2,
     marginVertical: 15,
     marginHorizontal: 30,
-    paddingBottom: 15}
+    paddingBottom: 15},
+  item: { borderColor: "black", borderWidth: 2,
+          backgroundColor: "#A0FFA0AA",
+          paddingHorizontal: 30,
+          paddingVertical: 5,
+          marginHorizontal: 20,
+          marginVertical: 5}
 })
 
 class DepesasFormulario extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {nome: "", valor: "", data: "", lista: []}
+    this.state = {nome: "", valor: "", data: "", lista: [
+      {nome: "Luz", valor: "300,00", data: "10/04/2023"},
+      {nome: "Água", valor: "200,00", data: "15/04/2023"}
+    ]}
   }
 
   render() {
+
+    const listaView = [];
+    for (o of this.state.lista) {
+      listaView.push(
+        <View style={estilos.item}>
+          <Text>Nome da despesa: {o.nome}</Text>
+          <Text>Valor: {o.valor}</Text>
+          <Text>Data Pagamento: {o.data}</Text>
+        </View>
+      );
+    }
+
     return (
       <View style={{flex: 1}}>
         <ImageBackground source={coinsimg}
@@ -63,7 +84,17 @@ class DepesasFormulario extends React.Component {
                       onChangeText={(txt)=>{
                         this.setState({data: txt})
                       }}/>
-          <Button title="Salvar" color="green"/>                                          
+          <Button title="Salvar" color="green" onPress={
+            ()=>{
+              const obj = { nome: this.state.nome,
+                            valor: this.state.valor,
+                            data: this.state.data }
+              // alert("Objeto: " + JSON.stringify(obj));
+              this.setState({lista: [... this.state.lista, obj]})
+            }}/>
+            <ScrollView style={{flex: 1}}>
+              {listaView}
+            </ScrollView>                                          
         </View>
       </View>
     );
