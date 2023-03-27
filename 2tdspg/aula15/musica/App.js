fd6768b08a0c9eb53de98f6dc2a001c3c46eb922
Antonio Carvalho <antoniorcn@hotmail.com>
2023-03-27 12:04:36 -0300

    2TDSPG Aula 15 parte 2

--- 2tdspg/aula15/musica/App.js
+++ 2tdspg/aula15/musica/App.js
@@ -1,14 +1,62 @@
 import React from 'react';
-import {ImageBackground, View, Text} from 'react-native';
+import {Button, ImageBackground, View,
+    ScrollView, Text, TextInput} from 'react-native';
 import imgMusica from './assets/musica.jpg'
 
 
-class MusicaFormulario extends React.Component { 
+class MusicaFormulario extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {album: "Get a Grip",
+      musica: "Amazing", banda: "Aerosmith", lista:[]};
+  }
+
   render() {
+
+    const listaVisual = [];
+    for (let i = 0; i < this.state.lista.length; i++) {
+      const o = this.state.lista[i];
+
+      listaVisual.push(
+          <View key={i} style={{backgroundColor: "lightyellow",
+          borderWidth: 2, margin: 3}}>
+            <Text>Album: {o.album}</Text>
+            <Text>Musica : {o.musica}</Text>
+            <Text>Banda: {o.banda}</Text>
+          </View>
+      );
+    }
+
     return (
       <View style={{flex: 1}}>
-        <View style={{flex: 1}}></View>
-        <View style={{flex: 1}}></View>
+        <View style={{flex: 1}}>
+          <Text>Formulario</Text>
+          <Text>Nome do Album</Text>
+          <TextInput value={this.state.album} 
+              onChangeText={(txt)=>{
+                this.setState({album: txt})
+              }}/>
+          <Text>Nome da Musica</Text>
+          <TextInput value={this.state.musica}
+              onChangeText={(txt)=>{
+                this.setState({musica: txt})
+              }}/>
+          <Text>Nome da Banda</Text>
+          <TextInput value={this.state.banda}
+              onChangeText={(txt)=>{
+                this.setState({banda: txt})
+              }}/>
+          <Button title="Salvar" onPress={()=>{
+            const obj = { album: this.state.album, 
+                          musica: this.state.musica,
+                          banda: this.state.banda }
+            this.setState({lista: [ ... this.state.lista, obj ] })
+          }}/>                 
+        </View>
+        <ScrollView style={{flex: 1}}>
+          <Text>Lista de Musicas</Text>
+          {listaVisual}
+        </ScrollView>
       </View>
     );
   }
@@ -21,6 +69,6 @@ export default () =>
       <Text>App de musicas</Text>
     </ImageBackground>
     <View style={{flex: 3}}>
-
+      <MusicaFormulario/>
     </View>
   </View>
