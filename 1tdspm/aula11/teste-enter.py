b96e84d63b60d3c4e5f901c77abb7e79f1ae4753
Antonio Carvalho <antoniorcn@hotmail.com>
2023-04-03 09:46:10 -0300

    Aula 11 1TDSPM

---
+++ 1tdspm/aula11/teste-enter.py
@@ -0,0 +1,6 @@
+print("Inicio do programa")
+
+for i in range(5):
+    print("*", end="")
+    
+print("Fim do programa")
---
+++ 1tdspm/aula11/teste-linhas.py
@@ -0,0 +1,6 @@
+# print(" ***\n*** ")
+# print("\t***\t***")
+# print("***")
+# \n - Enter
+# \t - Tab
+# \b - Backspace
