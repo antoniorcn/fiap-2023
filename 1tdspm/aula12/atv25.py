b4f8f5b290a47af4a37df574dc1d71982f8a5800
Antonio Carvalho <antoniorcn@hotmail.com>
2023-04-10 08:57:57 -0300

    Aula 12 1TDSPM parte 1

---
+++ 1tdspm/aula12/atv25.py
@@ -0,0 +1,20 @@
+print("Programa que calcula a media final")
+notas = []
+
+for i in range(6):
+    # cp1 = float(input("Por favor digite a nota do" + \
+    #                   "Checkpoint {}: ".format(i + 1)))
+    cp1 = float(input(f'Por favor digite a nota do Checkpoint {i + 1}: '))
+    notas.append(cp1)
+
+ch1 = float(input('Por favor digite agora a 1ª nota do Challenge'))
+notas.append(ch1)
+
+ch2 = float(input('Por favor digite agora a 2ª nota do Challenge'))
+notas.append(ch2)
+
+gs = float(input('Por favor digite agora a nota da Global Solution'))
+notas.append(gs)
+
+print("Lista de Notas: ")
+print(notas)
