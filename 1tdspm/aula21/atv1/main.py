d9eae5ac3211cdb4ce5aac80f0fc8ee88640d675
Antonio Carvalho <antoniorcn@hotmail.com>
2023-05-15 10:27:07 -0300

    1TDSPM Aula 21 parte 2

--- 1tdspm/aula21/atv1/main.py
+++ 1tdspm/aula21/atv1/main.py
@@ -8,22 +8,43 @@ def entrar_dados():
   return obj
 
 
-lista = [
-  {"nome": "João Silva", "telefone": "(11) 1234-5678", "email": "joao@teste.com"},
-  {"nome": "Maria Silva", "telefone": "(11) 3434-1212", "email": "maria@teste.com"},
-  {"nome": "Fabricio Orlando", "telefone": "(11) 4545-6767", "email": "fabricio@teste.com"},
-  {"nome": "Maria Silva", "telefone": "(11) 3434-1212", "email": "maria2@teste.com"},
-]
-
-while True:
+def menu_principal():
   print("Agenda de Contatos")
   print("(C) - Cadastrar")
   print("(L) - Listar")
   print("(P) - Procurar")
   print("(S) - Sair")
 
-  opcao = input("Digite sua opção").upper()[0]
+  opcao_escolhida = input("Digite sua opção").upper()[0]
+  return opcao_escolhida
 
+
+def menu_contato( contato ):
+  print("Contato encontrado:")
+  print(f"Nome:      {contato['nome']:<40}")
+  print(f"Telefone:  {contato['telefone']:<40}")
+  print(f"eMail:     {contato['email']:<40}")
+  print(f"Nascimento:     {contato['nascimento']:<40}")
+  print("Informe o que você deseja fazer com este contato: ")
+  print("(A) - Apagar")
+  print("(E) - Editar")
+  print("(V) - Voltar ao menu principal")
+  opcao_contato_escolhido = input("Escolha sua opção:").upper()[0]
+  return opcao_contato_escolhido
+
+lista = [
+  {"nome": "João Silva", "telefone": "(11) 1234-5678", 
+    "email": "joao@teste.com", "nascimento": "07/09/2000"},
+  {"nome": "Maria Silva", "telefone": "(11) 3434-1212", 
+    "email": "maria@teste.com", "nascimento": "15/04/1998"},
+  {"nome": "Fabricio Orlando", "telefone": "(11) 4545-6767",
+    "email": "fabricio@teste.com", "nascimento": "11/01/1994"},
+  {"nome": "Maria Silva", "telefone": "(11) 3434-1212",
+    "email": "maria2@teste.com", "nascimento": "23/11/2002"},
+]
+
+while True:
+  opcao = menu_principal()
   if opcao == 'C':
     contato = entrar_dados()
     lista.append(contato)
@@ -32,15 +53,7 @@ while True:
     i = 0
     for cont in lista:
       if cont["nome"] == nome_procurar:
-        print("Contato encontrado:")
-        print(f"Nome:      {cont['nome']:<40}")
-        print(f"Telefone:  {cont['telefone']:<40}")
-        print(f"eMail:     {cont['email']:<40}")
-        print("Informe o que você deseja fazer com este contato: ")
-        print("(A) - Apagar")
-        print("(E) - Editar")
-        print("(V) - Voltar ao menu principal")
-        opcao_contato = input("Escolha sua opção:").upper()[0]
+        opcao_contato = menu_contato( cont )
         if opcao_contato == 'A':
           lista.remove(cont)
           print("Contato apagado com sucesso")
---
+++ 1tdspm/aula21/atv1/teste_funcoes2.py
@@ -0,0 +1,9 @@
+def soma_numeros(n1, n2):
+    return n1 + n2
+
+
+a = int(input("Por favor digite um numero: "))
+b = int(input("Por favor digite outro numero: "))
+valor = soma_numeros(a, b)
+
+print("Valor: ", valor)
