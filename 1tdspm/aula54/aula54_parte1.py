1d5ae30d4da1a54f14dda798d3eee43daa486ff2
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-09-21 12:01:49 -0300

    aula 54 - parte 1

---
+++ 1tdspm/aula54/aula54_parte1.py
@@ -0,0 +1,39 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+class MeuApp( App ): 
+    def build(self):
+        box_principal = BoxLayout(orientation="vertical")
+
+        box_nome = BoxLayout(orientation="horizontal")
+        txt_nome = TextInput()
+        box_nome.add_widget( Label(text="Nome: ") )
+        box_nome.add_widget(txt_nome)
+
+        box_telefone = BoxLayout(orientation="horizontal")
+        txt_telefone = TextInput()
+        box_telefone.add_widget( Label(text="Telefone: ") )
+        box_telefone.add_widget(txt_telefone)
+
+        box_email = BoxLayout(orientation="horizontal")
+        txt_email = TextInput()
+        box_email.add_widget( Label(text="Email: ") )
+        box_email.add_widget(txt_email) 
+
+        box_botoes = BoxLayout(orientation="horizontal")
+        btn_salvar = Button(text="Salvar")
+        btn_listar = Button(text="Listar")
+        box_botoes.add_widget(btn_salvar)
+        box_botoes.add_widget(btn_listar)
+
+        box_principal.add_widget(box_nome)
+        box_principal.add_widget(box_telefone)
+        box_principal.add_widget(box_email)
+        box_principal.add_widget(box_botoes)
+
+        return box_principal
+
+MeuApp().run()
